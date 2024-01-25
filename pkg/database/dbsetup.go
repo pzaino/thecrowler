442295c1a0429888cf2ad0b9029a59df94720d21
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2024-01-25 20:35:51 +0000

    fixed wrong go doc syntax for the packages

--- pkg/database/dbsetup.go
+++ pkg/database/dbsetup.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// The database package is responsible for handling the database
+// Package database is responsible for handling the database
 // setup and configuration.
 package database
 
