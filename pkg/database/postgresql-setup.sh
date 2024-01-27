f0f63f6620f2269420e146232afbb962c3597dbc
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2024-01-27 17:46:50 +0000

    reintroduced the setup script in pgsql and fixed a bug in the search engine that was preventing dorking techniques to work correctly

---
+++ pkg/database/postgresql-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Perform all actions as $DOCKER_POSTGRES_USER
+export PGPASSWORD="$POSTGRES_PASSWORD"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" \
+     -v POSTGRES_DB="$POSTGRES_DB" \
+     -v CROWLER_DB_USER="$CROWLER_DB_USER" \
+     -v CROWLER_DB_PASSWORD="$CROWLER_DB_PASSWORD" \
+     -f postgres-setup-v1.1.pgsql
