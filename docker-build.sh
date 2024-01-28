369c48b6060a51da416b802630647d7068f55a35
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2024-01-28 17:46:02 +0000

    Removed all default passwords and added errors for when user's pwd are not set at build time

--- docker-build.sh
+++ docker-build.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
-
-if [ "${DOCKER_DB_PORT}" = "" ]; then
-    export DOCKER_DB_PORT=5432
-fi
+# Check for mandatory settings
 
 # shellcheck disable=SC2153
 if [ "${DOCKER_DB_HOST}" = "" ]; then
-    echo "DOCKER_DB_HOST is not set"
+    echo "DOCKER_DB_HOST is not set!"
     exit 1
+fi
+
+if [ "${DOCKER_POSTGRES_DB_PASSWORD}" = "" ]; then
+    echo "DOCKER_POSTGRES_DB_PASSWORD is not set!"
+    exit 1
+fi
+
+if [ "${DOCKER_CROWLER_DB_PASSWORD}" = "" ]; then
+    echo "DOCKER_CROWLER_DB_PASSWORD is not set!"
+    exit 1
+fi
+
+# Check for optional settings
+
+if [ "${DOCKER_DB_PORT}" = "" ]; then
+    export DOCKER_DB_PORT=5432
 fi
 
 if [ "${DOCKER_POSTGRES_DB_USER}" = "" ]; then
     export DOCKER_POSTGRES_DB_USER=postgres
 fi
 
-if [ "${DOCKER_POSTGRES_DB_PASSWORD}" = "" ]; then
-    export DOCKER_POSTGRES_DB_PASSWORD=postgres
-fi
-
 if [ "${DOCKER_POSTGRES_DB_NAME}" = "" ]; then
     export DOCKER_POSTGRES_DB_NAME=SitesIndex
+fi
+
+if [ "${DOCKER_CROWLER_DB_USER}" = "" ]; then
+    export DOCKER_CROWLER_DB_USER=crowler
 fi
 
 # Detect host architecture
