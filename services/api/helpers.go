5a67feaf043becbe131b58be5c410e972fea1907
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2024-02-16 14:51:19 +0000

    removed a debug line from code

--- services/api/helpers.go
+++ services/api/helpers.go
@@ -11,7 +11,6 @@ import (
 
 // handleErrorAndRespond encapsulates common error handling and JSON response logic.
 func handleErrorAndRespond(w http.ResponseWriter, err error, results interface{}, errMsg string, errCode int) {
-	fmt.Printf("Results: %v\n", results)
 	if err != nil {
 		cmn.DebugMsg(cmn.DbgLvlDebug3, errMsg, err)
 		http.Error(w, err.Error(), errCode)
