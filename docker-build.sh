9d6f17705262b10611dc7594c7c04f34c5232d7c
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2024-04-28 21:28:32 +0100

    Initial changes to build the customized selenium container

--- docker-build.sh
+++ docker-build.sh
@@ -46,11 +46,51 @@ if [ "$ARCH" = "aarch64" ] || [ "$ARCH" = "arm64" ]; then
     POSTGRES_IMAGE="arm64v8/"
     SELENIUM_IMAGE="seleniarm/standalone-chromium"
 fi
+if [ "$SELENIUM_RELEASE" == "" ];
+then
+    #SELENIUM_RELEASE=4.18.1-20240224
+    SELENIUM_RELEASE=4.20.0-20240428
+fi
+if [ "$SELENIUM_PORT" == "" ];
+then
+    SELENIUM_PORT=4444
+fi
 
 # Export platform as an environment variable
 export DOCKER_DEFAULT_PLATFORM=$PLATFORM
 export DOCKER_POSTRGESS_IMAGE=$POSTGRES_IMAGE
 export DOCKER_SELENIUM_IMAGE=$SELENIUM_IMAGE
+
+# Build custom Selenium image
+echo "Building the selenium container"
+rm -rf docker-selenium
+git clone git@github.com:SeleniumHQ/docker-selenium.git ./docker-selenium
+rval=$?
+if [ $rval -ne 0 ]; then
+    echo "Failed to clone the Selenium repository"
+    exit 1
+fi
+# Prepare Sources
+cd ./docker-selenium || exit 1
+git checkout "${SELENIUM_RELEASE}"
+git pull origin "${SELENIUM_RELEASE}"
+# patch Selenium Dockefile and start-selenium-standalone.sh files:
+cp ../selenium-patches/Dockerfile ./Standalone/Dockerfile
+cp ../selenium-patches/start-selenium-standalone.sh ./Standalone/start-selenium-standalone.sh
+mkdir -p ./Standalone/Rbee
+cp -r ../cmd ./Standalone/Rbee/cmd
+cp -r ../pkg ./Standalone/Rbee/pkg
+cp -r ../services ./Standalone/Rbee/services
+cp -r ../schemas ./Standalone/Rbee/schemas
+cp -r ../go.mod ./Standalone/Rbee/go.mod
+cp -r ../go.sum ./Standalone/Rbee/go.sum
+cp -r ../config.sh ./Standalone/Rbee/config.yaml
+cp ../main.go ./Standalone/Rbee/main.go
+cp ../selenium-patches/browserAutomation.conf ./Standalone/Rbee/browserAutomation.conf
+cp ../autobuild.sh ./Standalone/Rbee/autobuild.sh
+
+# build Selenium image
+make standalone_chrome
 
 # Run Docker Compose
 docker-compose up --build
