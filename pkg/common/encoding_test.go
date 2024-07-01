547bb55108577b35cfc8894ef9b82a3fcbae00d7
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2024-07-01 19:34:59 +0100

    Added more unit tests

--- pkg/common/encoding_test.go
+++ pkg/common/encoding_test.go
@@ -23,3 +23,15 @@ func TestCalculateEntropy(t *testing.T) {
 		t.Errorf("CalculateEntropy(%s) = %f, expected %f", data, resultCheck, expectedCheck)
 	}
 }
+
+func TestBase64Decode(t *testing.T) {
+	data := "SGVsbG8sIFdvcmxkIQ=="
+	expected := "Hello, World!"
+	result, err := Base64Decode(data)
+	if err != nil {
+		t.Errorf("Base64Decode(%s) returned an error: %v", data, err)
+	}
+	if result != expected {
+		t.Errorf("Base64Decode(%s) = %s, expected %s", data, result, expected)
+	}
+}
