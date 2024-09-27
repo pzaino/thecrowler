5a0caa1b617ec0e0b7e3fa1fce3b9edcef4e26d5
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2024-09-28 00:52:58 +0100

    Improved API perf tests

--- tests/perf/api_get_healthcheck.js
+++ tests/perf/api_get_healthcheck.js
@@ -5,7 +5,7 @@ const test_target = 1000;
 
 export let options = {
   stages: [
-    { duration: '10s', target: 100 },  // Ramp-up to 100 VUs over 10 seconds
+    { duration: '10s', target: test_target / 10 },  // Ramp-up to 100 VUs over 10 seconds
     { duration: '30s', target: test_target }, // Then sustain test_target VUs for 30 seconds
     { duration: '10s', target: 0 },    // Ramp-down to 0 VUs
   ],
@@ -19,7 +19,7 @@ export let options = {
  * having the DB queries in the way.
  */
 export default function () {
-  let res = http.get('http://localhost:8080/v1/health');
+  let res = http.get('http://localhost:8080/v1/health', { timeout: '10s' });
   check(res, {
     'is status 200': (r) => r.status === 200,
   });
