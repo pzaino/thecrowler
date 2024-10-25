e88a9adc8be24a8a71c33fc149245ca758e4bc97
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2024-10-25 23:31:37 +0100

    Added new consts file

---
+++ pkg/crawler/consts.go
@@ -0,0 +1,42 @@
+// Copyright 2023 Paolo Fabio Zaino
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package crawler implements the crawler library for the Crowler
+package crawler
+
+const (
+	strPluginCall       = "plugin_call"
+	strRegEx            = "regex"
+	strXPath            = "xpath"
+	strCSS              = "css"
+	strJSON             = "json"
+	strYAML             = "yaml"
+	strText1            = "text"
+	strText2            = "text_content"
+	strText3            = "inner_text"
+	strText4            = "html"
+	strJSPath           = "js_path"
+	strName             = "name"
+	strLinkText1        = "link_text"
+	strLinkText2        = "linktext"
+	strPartialLinkText1 = "partial_link_text"
+	strPartialLinkText2 = "partiallinktext"
+	strTagName1         = "tag_name"
+	strTagName2         = "tagname"
+	strTagName3         = "tag"
+	strTagName4         = "element"
+	strClassName1       = "class_name"
+	strClassName2       = "classname"
+	strClassName3       = "class"
+)
