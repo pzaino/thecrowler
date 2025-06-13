b0c03b9369165c697281513465a43209c1606225
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2025-06-13 21:11:17 +0100

    Made sure it doesn't log contantly  when there are no sources to crawl and we are in debug mode

--- main.go
+++ main.go
@@ -261,6 +261,8 @@ func checkSources(db *cdb.Handler, sel *vdi.Pool, RulesEngine *rules.RuleEngine)
 	maintenanceTime := time.Now().Add(time.Duration(config.Crawler.Maintenance) * time.Minute)
 	// Set the resource release time
 	resourceReleaseTime := time.Now().Add(time.Duration(5) * time.Minute)
+	// Flag used to avoid repeating 0 sources found constantly
+	gotSources := true // set to true by default for first source check (so it will log the first time)
 
 	// Start the main loop
 	defer configMutex.RUnlock()
@@ -279,7 +281,10 @@ func checkSources(db *cdb.Handler, sel *vdi.Pool, RulesEngine *rules.RuleEngine)
 
 		// Check if there are sources to crawl
 		if len(sourcesToCrawl) == 0 {
-			cmn.DebugMsg(cmn.DbgLvlDebug4, "No sources to crawl, sleeping...")
+			if gotSources {
+				cmn.DebugMsg(cmn.DbgLvlDebug2, "No sources to crawl, sleeping...")
+				gotSources = false
+			}
 
 			// Perform database maintenance if it's time
 			if time.Now().After(maintenanceTime) {
@@ -298,7 +303,8 @@ func checkSources(db *cdb.Handler, sel *vdi.Pool, RulesEngine *rules.RuleEngine)
 			time.Sleep(time.Duration(config.Crawler.QueryTimer) * time.Second)
 			continue
 		}
-		cmn.DebugMsg(cmn.DbgLvlDebug4, "Sources to crawl: %d", len(sourcesToCrawl))
+		cmn.DebugMsg(cmn.DbgLvlDebug2, "Sources to crawl: %d", len(sourcesToCrawl))
+		gotSources = true
 
 		// Crawl each source
 		workBlock := WorkBlock{
