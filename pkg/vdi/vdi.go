2e9c9d278f139c8d248e21533e16e3f83ba48a41
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2025-08-27 23:37:32 +0100

    Improved error handling in the VDICOnnet

--- pkg/vdi/vdi.go
+++ pkg/vdi/vdi.go
@@ -474,6 +474,8 @@ func ConnectVDI(ctx ProcessContextInterface, sel SeleniumInstance, browseType in
 	if ctx == nil {
 		return nil, errors.New("context is nil")
 	}
+
+	// Validate the VDI configuration
 	if sel.Config.Host == "" {
 		return nil, errors.New("VDI instance host is empty")
 	}
@@ -871,6 +873,10 @@ func ConnectVDI(ctx ProcessContextInterface, sel SeleniumInstance, browseType in
 	result, err2 := getBrowserConfiguration(&wd)
 	if err2 != nil {
 		cmn.DebugMsg(cmn.DbgLvlError, "Error executing script: %v\n", err)
+		// Check if the error contains: "invalid session id"
+		if strings.Contains(err2.Error(), "invalid session id") {
+			return nil, fmt.Errorf("VDI session is invalid, something closed it, cannot continue")
+		}
 	} else {
 		cmn.DebugMsg(cmn.DbgLvlDebug, "Browser Configuration: %v\n", result)
 	}
@@ -878,6 +884,10 @@ func ConnectVDI(ctx ProcessContextInterface, sel SeleniumInstance, browseType in
 	err2 = addLoadListener(&wd)
 	if err2 != nil {
 		cmn.DebugMsg(cmn.DbgLvlError, "adding Load Listener to the VDI session: %v", err)
+		// Check if the error contains: "invalid session id"
+		if strings.Contains(err2.Error(), "invalid session id") {
+			return nil, fmt.Errorf("VDI session is invalid, something closed it, cannot continue")
+		}
 	}
 
 	// Configure CDP
@@ -903,6 +913,10 @@ func ConnectVDI(ctx ProcessContextInterface, sel SeleniumInstance, browseType in
 		_, err2 := wd.ExecuteChromeDPCommand("Network.enable", nil)
 		if err2 != nil {
 			cmn.DebugMsg(cmn.DbgLvlError, "Failed to enable CDP Network domain: %v", err2)
+			// Check if the error contains: "invalid session id"
+			if strings.Contains(err2.Error(), "invalid session id") {
+				return nil, fmt.Errorf("VDI session is invalid, something closed it, cannot continue")
+			}
 		} else {
 			_, err2 = wd.ExecuteChromeDPCommand("Network.setBlockedURLs", map[string]interface{}{
 				"urls": []string{"*.mp4"},
